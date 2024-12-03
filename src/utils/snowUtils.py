daf0e1d8ff894ec5ca18804fcd71758834e3807a
prash-code <prashantp7472@gmail.com>
2024-12-03 23:51:00 +0530

    Add all credentials

--- src/utils/snowUtils.py
+++ src/utils/snowUtils.py
@@ -1,4 +1,4 @@
-import snowflake.connector 
+import snowflake.connector
 
 class SnowflakeConnector:
     def __init__(self, account, user, password, warehouse, database, schema, role):
@@ -9,16 +9,18 @@ class SnowflakeConnector:
         self.database = database
         self.schema = schema
         self.role = role
-    
+        self.connection = None
+        self.cursor = None
+
     def connect(self):
-        snowflake.connection = snowflake.connector.connect(
-            user = self.user,
-            password = self.password,
-            account = self.account,
-            warehouse = self.warehouse,
-            database = self.database,
-            schema = self.schema,
-            role = self.role
+        self.connection = snowflake.connector.connect(
+            user=self.user,
+            password=self.password,
+            account=self.account,
+            warehouse=self.warehouse,
+            database=self.database,
+            schema=self.schema,
+            role=self.role
         )
         self.cursor = self.connection.cursor()
 
@@ -27,18 +29,22 @@ class SnowflakeConnector:
         return self.cursor.fetchall()
     
     def close_connection(self):
-        self.cursor.close()
-        self.connection.close()
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
 
 
+# Parameters (Ideally should be loaded from environment variables or a config file)
 account = "MGNKVYR-JF04384"
 user = "PRASHANTY"
 password = "Prashant$00"
 database = "SNOWFLAKE_SAMPLE_DATA"
-schema = "TPCH_SF10"
+schema = "TPCH_SF1"
 warehouse = "COMPUTE_WH"
 role = "ACCOUNTADMIN"
 
+# Usage
 sf_connector = SnowflakeConnector(
     account=account,
     user=user,
@@ -49,11 +55,11 @@ sf_connector = SnowflakeConnector(
     role=role
 )
 
-sf_connector.connect()
-
-query = "select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.SUPPLIER limit 10"
-result = sf_connector.execute_query(query)
-
-print(result)
-
-sf_connector.close_connection()
+try:
+    sf_connector.connect()
+    query = "SELECT * FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.SUPPLIER LIMIT 10"
+    result = sf_connector.execute_query(query)
+    for row in result:
+        print(row)
+finally:
+    sf_connector.close_connection()
