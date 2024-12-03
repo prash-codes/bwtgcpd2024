aa2e4e8f74092cfdb208c00572916fc73721901f
prash-code <prashantp7472@gmail.com>
2024-12-03 14:22:23 +0530

    Make connection with snowflake

---
+++ src/utils/snowUtils.py
@@ -0,0 +1,59 @@
+import snowflake.connector # type: ignore
+
+class SnowflakeConnector:
+    def __init__(self, account, user, password, warehouse, database, schema, role):
+        self.account = account
+        self.user = user
+        self.password = password
+        self.warehouse = warehouse
+        self.database = database
+        self.schema = schema
+        self.role = role
+    
+    def connect(self):
+        snowflake.connection = snowflake.connector.connect(
+            user = self.user,
+            password = self.password,
+            account = self.account,
+            warehouse = self.warehouse,
+            database = self.database,
+            schema = self.schema,
+            role = self.role
+        )
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
+
+
+account = "MGNKVYR-JF04384"
+user = "PRASHANTY"
+password = "Prashant$00"
+database = "SNOWFLAKE_SAMPLE_DATA"
+schema = "TPCH_SF10"
+warehouse = "COMPUTE_WH"
+role = "ACCOUNTADMIN"
+
+sf_connector = SnowflakeConnector(
+    account=account,
+    user=user,
+    password=password,
+    database=database,
+    schema=schema,
+    warehouse=warehouse,
+    role=role
+)
+
+sf_connector.connect()
+
+query = "select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.SUPPLIER"
+result = sf_connector.execute_query(query)
+
+print(result)
+
+sf_connector.close_connection()
